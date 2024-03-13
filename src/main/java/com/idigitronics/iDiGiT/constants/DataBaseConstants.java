ce6c4197ce834144aec4c16b66b09d664aa4b748
vml <vml@192.168.1.13>
2024-03-13 19:55:10 +0530

    mac db password

--- src/main/java/com/idigitronics/iDiGiT/constants/DataBaseConstants.java
+++ src/main/java/com/idigitronics/iDiGiT/constants/DataBaseConstants.java
@@ -8,10 +8,10 @@ package com.idigitronics.iDiGiT.constants;
 public class DataBaseConstants {
 
 	public static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
-	public static final String DRIVER_URL = "jdbc:mysql://localhost:3306/idigitest";
+//	public static final String DRIVER_URL = "jdbc:mysql://localhost:3306/idigitest";
 //	public static final String DRIVER_URL = "jdbc:mysql://49.207.3.214:3306/idigi";
-//	public static final String DRIVER_URL = "jdbc:mysql://localhost:3306/idigitest?useSSL=false";
+	public static final String DRIVER_URL = "jdbc:mysql://localhost:3306/idigitest?useSSL=false";
 	public static final String USER_NAME = "root";
-//	public static final String PASSWORD = "lamiv9889";
-	public static final String PASSWORD = "root";
+	public static final String PASSWORD = "lamiv@9889";
+//	public static final String PASSWORD = "root";
 }
